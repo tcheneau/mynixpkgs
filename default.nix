35c77ec5f2a0ea9108114f97b4dc4954163c8519
Tony Cheneau <tony.cheneau@amnesiak.org>
2021-01-11 00:47:28 +0100

    entirely suppres pkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz) {},
   ...
 }:
 
@@ -9,7 +8,7 @@ let
   packages = rec {
     podman_compose = python3Packages.callPackage ./pkgs/podman-compose {};
 
-    inherit pkgs; # similar to `pkgs = pkgs;` This lets callers use the nixpkgs version defined in this file.
+    #inherit pkgs; # similar to `pkgs = pkgs;` This lets callers use the nixpkgs version defined in this file.
   };
 in
   packages
