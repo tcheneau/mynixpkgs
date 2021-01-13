b5d4a10615310c63c082f964c43c12462372d1fa
Tony Cheneau <tony.cheneau@amnesiak.org>
2021-01-13 23:22:24 +0100

    Add non fonctionnal zed-free
    It begins to load and then stops because it fetches dependencies from
    /usr/share.

--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@ self: super:
 
 {
    podman_compose = super.python3Packages.callPackage ./pkgs/podman-compose {};
+   zed_free = super.callPackage ./pkgs/zed-free {};
 }
---
+++ pkgs/zed-free/default.nix
@@ -0,0 +1,68 @@
+{ stdenv, lib, fetchurl, makeWrapper
+, dpkg, patchelf
+, gtk2, glib, gdk-pixbuf, alsaLib, nss, nspr, cups, libgcrypt, dbus, systemd
+, libXdamage, expat
+, qt5, libxml2, libxslt, openldap }:
+
+let
+  inherit (stdenv) lib;
+  LD_LIBRARY_PATH = lib.makeLibraryPath
+    [ glib gtk2 gdk-pixbuf alsaLib nss nspr cups libgcrypt qt5.qtbase libxml2 libxslt openldap dbus libXdamage expat ];
+in
+stdenv.mkDerivation rec {
+  version = "2020.4";
+  pname = "zed-free";
+
+  src =
+    if stdenv.hostPlatform.system == "i686-linux" then fetchurl {
+      url = "https://www.zedencrypt.com/file/get/-/item_key/13802-21-57cde2c4";
+      sha256 = "0hv1x2lhdmrwzckqiz0lrg95mb49f9099mgwp5f7zjp5kb4llyxs";
+    } else fetchurl {
+      url = "https://www.zedencrypt.com/file/get/-/item_key/13802-22-7036b18e";
+      sha256 = "16gql2a9ld8p6b28pw4ix2jg0ikhpx96nin8w6cvd026cr23x2ng";
+    };
+
+  nativeBuildInputs = [ makeWrapper dpkg ];
+
+  unpackPhase = ''
+    mkdir pkg
+    dpkg-deb -x $src pkg
+    sourceRoot=pkg
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mv usr/bin/zed_free $out/bin
+
+    mkdir -p $out/share
+    mv usr/share/* $out/share
+    
+    mkdir -p $out/lib
+    ln -s ${stdenv.cc.cc.lib}/lib/libstdc++.so.6 $out/lib/
+#    ln -s ${lib.getLib systemd}/lib/libudev.so.1 $out/lib/libudev.so.0
+
+    ${patchelf}/bin/patchelf \
+      --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      $out/bin/zed_free
+     wrapProgram $out/bin/zed_free \
+       --prefix QT_PLUGIN_PATH : ${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix} \
+#       --prefix QT_DEBUG_PLUGINS 1 \
+       --prefix LD_LIBRARY_PATH : $out/lib:${LD_LIBRARY_PATH}
+
+#
+#    for binary in StarUML Brackets-node; do
+#      ${patchelf}/bin/patchelf \
+#        --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+#        $out/bin/$binary
+#      wrapProgram $out/bin/$binary \
+#        --prefix LD_LIBRARY_PATH : $out/lib:${LD_LIBRARY_PATH}
+#    done
+  '';
+
+  meta = with stdenv.lib; {
+    description = "To encrypt personnal and sensitive data";
+    homepage = "https://www.zedencrypt.com/";
+    license = licenses.unfree;
+    platforms = [ "i686-linux" "x86_64-linux" ];
+  };
+}
