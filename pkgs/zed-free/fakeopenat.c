eefcf3b41d704aa82caa418212c4635776d74d77
Tony Cheneau <tony.cheneau@amnesiak.org>
2021-01-18 23:41:38 +0100

    refactoring

--- pkgs/zed-free/fakeopenat.c
+++ pkgs/zed-free/fakeopenat.c
@@ -23,23 +23,20 @@ nl_catd catopen(const char *name, int flag) {
 	memset(buf, 0, BUF_SIZE);
 
 	orig_catopen_f_type orig_catopen;
-	printf("meh\n");
     orig_catopen = (orig_catopen_f_type)dlsym(RTLD_NEXT,"catopen");
-	printf("moh\n");
 
-	if (prefix == NULL || strlen(name) < sizeof(HARD_PREFIX))
+	if (prefix == NULL ||
+	    strlen(name) < sizeof(HARD_PREFIX) ||
+	    memcmp(name, HARD_PREFIX, sizeof(HARD_PREFIX) -1) != 0
+	   )
 	    return orig_catopen(name, flag);
 
+	// printf("path: %s prefix: %s %d\n", name, HARD_PREFIX, sizeof(HARD_PREFIX));
+	// printf("memcmp %d\n", (memcmp(name, HARD_PREFIX, sizeof(HARD_PREFIX) -1)));
 
-	printf("path: %s prefix: %s %d\n", name, HARD_PREFIX, sizeof(HARD_PREFIX));
-	printf("memcmp %d\n", (memcmp(name, HARD_PREFIX, sizeof(HARD_PREFIX) -1)));
-
-	if (memcmp(name, HARD_PREFIX, sizeof(HARD_PREFIX) -1) == 0) {
-		int prefix_len = strnlen(prefix, 256);
-		memmove(buf, prefix, prefix_len);
-		memmove(buf + prefix_len, name + sizeof(HARD_PREFIX), strlen(name) - sizeof(HARD_PREFIX));
-		printf("changing path for %s (new path %s)\n", name, buf);
-	    return orig_catopen(buf, flag);
-	} else
-	    return orig_catopen(name, flag);
+	int prefix_len = strnlen(prefix, 256);
+	memmove(buf, prefix, prefix_len);
+	memmove(buf + prefix_len, name + sizeof(HARD_PREFIX), strlen(name) - sizeof(HARD_PREFIX));
+	printf("changing path for %s (new path %s)\n", name, buf);
+	return orig_catopen(buf, flag);
 }
