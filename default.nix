26cd007954f043c7398aa1f0d5449ba4f1fe0e58
Tony Cheneau <tony.cheneau@amnesiak.org>
2023-03-21 22:51:52 +0100

    Initial commit

--- default.nix
+++ default.nix
@@ -9,6 +9,7 @@ self: super:
    sysinfo = super.callPackage ./pkgs/sysinfo {};
    gorfplayer = super.callPackage ./pkgs/gorfplayer {};
    openldap24 = super.callPackage ./pkgs/openldap24 {};
+   samba4-fix = super.callPackage ./pkgs/samba {};
 #   imhex = super.callPackage ./pkgs/imhex {};
 #   anki = super.python3Packages.callPackage ./pkgs/anki {};
 }
